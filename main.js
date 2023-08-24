24ee634296f460478bc9a2d1c5314fc843bd3319
Rodrigo Facundo <frodzek@proton.me>
2023-08-23 22:02:40 -0300

    segundo commit

--- main.js
+++ main.js
@@ -4,9 +4,28 @@ const itemXMR = document.querySelector('#item-xmr');
 
 miBoton.addEventListener('click', () => {
     mostrarRectanguloYItemXMR();
+    agregarAlCarrito();
 });
 
 function mostrarRectanguloYItemXMR() {
     rectangulo.style.display = 'block'; // Muestra el rectángulo
     itemXMR.style.display = 'block'; // Muestra el item XMR
+
+    // Puedes agregar cualquier otra lógica aquí cuando se muestra el artículo XMR
+}
+
+function agregarAlCarrito() {
+    const item = {
+        nombre: 'Monero (XMR)',
+        precio: 220
+    };
+
+    // Obtén el carrito actual del localStorage
+    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+    // Agrega el nuevo item al carrito
+    carrito.push(item);
+
+    // Guarda el carrito actualizado en el localStorage
+    localStorage.setItem('carrito', JSON.stringify(carrito));
 }
